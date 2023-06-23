338b4f9c4b4228cc044088df7dabdacd494f5252
priyatam0509 <piyushpriya34@gmail.com>
2023-06-23 13:27:29 +0530

    gmail addition

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,9 +1,14 @@
 const nodemailer = require("nodemailer");
-
+// smtp-Mail.outlook.com 587
 const sendEmail = async (subject, message, send_to, sent_from, reply_to, attachments =[]) => {
   const transporter = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST,
-    port: "465",
+  service: 'gmail',
+    // host: process.env.EMAIL_HOST,
+    // port: "465",
+  host: 'smtp.gmail.com',
+  // host: process.env.EMAIL_HOST,
+  port: 587,
+  secure: false,
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
